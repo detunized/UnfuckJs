5e1412d0f8fcc7b2d82f5970f9bff7e6ee6808ca
Dmitry Yakimenko <detunized@gmail.com>
2015-01-02 14:46:22 +0100

    return a, b; -> a; return b;

--- src/net/detunized/unfuckjs/SplitCommaExpression.kt
+++ src/net/detunized/unfuckjs/SplitCommaExpression.kt
@@ -6,10 +6,12 @@ import com.intellij.lang.javascript.psi.JSBlockStatement
 import com.intellij.lang.javascript.psi.JSStatement
 import org.intellij.idea.lang.javascript.psiutil.JSElementFactory
 import com.intellij.openapi.ui.Messages
+import com.intellij.lang.javascript.psi.JSReturnStatement
 
 public class SplitCommaExpression: StatementIntentionAction() {
     override val name = "Split comma expression"
 
+    // TODO: Remove copy paste
     override fun invoke(statement: JSStatement) {
         fun doIt(s: JSExpressionStatement, e: JSCommaExpression) {
             val lhs = e.getLOperand().getText() + ';'
@@ -19,19 +21,39 @@ public class SplitCommaExpression: StatementIntentionAction() {
                 val newLhs = JSElementFactory.replaceStatement(s, lhs)
                 JSElementFactory.addStatementAfter(newLhs, rhs)
             } else {
-                Messages.showInfoMessage("Only works in the block at the moment", getText())
+                Messages.showInfoMessage(
+                        "Only works in the block at the moment.\n" +
+                                "Use built-in quick fix to add braces first.",
+                        getText()
+                )
+            }
+        }
+
+        fun doIt(s: JSReturnStatement, e: JSCommaExpression) {
+            val lhs = e.getLOperand().getText() + ';'
+            val rhs = "return " + e.getROperand().getText() + ';'
+
+            if (s.getParent() is JSBlockStatement) {
+                val newLhs = JSElementFactory.replaceStatement(s, lhs)
+                JSElementFactory.addStatementAfter(newLhs, rhs)
+            } else {
+                Messages.showInfoMessage(
+                        "Only works in the block at the moment.\n" +
+                                "Use built-in quick fix to add braces first.",
+                        getText()
+                )
             }
         }
 
         when (statement) {
             is JSExpressionStatement -> doIt(statement, statement.getExpression() as JSCommaExpression)
-            //is JSExpressionStatement -> doIt(s, s.getExpression() as JSCommaExpression)
+            is JSReturnStatement -> doIt(statement, statement.getExpression() as JSCommaExpression)
         }
     }
 
     override fun isAvailable(statement: JSStatement) = when (statement) {
         is JSExpressionStatement -> statement.getExpression() is JSCommaExpression
-        //is JSReturnStatement -> s.getExpression() is JSCommaExpression
+        is JSReturnStatement -> statement.getExpression() is JSCommaExpression
         else -> false
     }
 }
